d4fa55761e92d715bbb15bfb5bf06b987f94a721
Zac <zombiezac@att.net>
2019-02-04 13:08:56 -0700

    completed frame class

--- src/pokemon/view/PokedexFrame.java
+++ src/pokemon/view/PokedexFrame.java
@@ -1,6 +1,29 @@
 package pokemon.view;
 
-public class PokedexFrame 
+import javax.swing.JFrame;
+import pokemon.controller.PokedexController;
+
+public class PokedexFrame extends JFrame 
 {
+	private PokedexController app;
+	private PokedexPanel panel;
 	
+	public PokedexFrame(PokedexController app)
+	{
+		super();
+		
+		this.app = app;
+		this.panel = new PokedexPanel(app);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(panel);
+		this.setTitle("Pokedex");
+		this.setSize(1000, 500);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -1,6 +1,28 @@
 package pokemon.view;
 
-public class PokedexPanel
+import javax.swing.*;
+import pokemon.controller.PokedexController;
+import java.awt.event.*;
+
+public class PokedexPanel extends JPanel
 {
+	private PokedexController app;
+	private JLabel dragoniteLabel;
+	private JLabel articunoLabel;
+	private JLabel arcanineLabel;
+	private JLabel honedgeLabel;
+	private JLabel lampentLabel;
+	private JTextField DragoniteText;
+	private JTextField ArticunoText;
+	private JTextField ArcanineText;
+	private JTextField HonedgeText;
+	private JTextField LampentText;
+	private SpringLayout appLayout;
 	
+	public PokedexPanel(PokedexController app)
+	{
+		super();
+		
+		
+	}
 }
